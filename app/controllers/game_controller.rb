bd2d60b0c29cbe15e7732038bb82eb8395eece6f
Anthony Sorise <contact@anthonysorise.com>
2024-06-09 18:39:05 -0400

    controller fix and add view

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,14 @@
 class GameController < ApplicationController
     def index
       session[:target] ||= rand(1..100)
-      session[:message] = "Guess a number between 1 and 100"
+      session[:message] ||= "Guess a number between 1 and 100"
+
     end
   
     def guess
       target = session[:target]
       guess = params[:guess].to_i
-  
+
       if guess < target
         session[:message] = "Too low! Try again."
       elsif guess > target
@@ -16,7 +17,7 @@ class GameController < ApplicationController
         session[:message] = "You got it! The number was #{target}. Play again!"
         session[:target] = rand(1..100) # Reset the game
       end
-  
+
       redirect_to root_path
     end
-  end
+end
